905ce29b67036fe5de0cfe5d0b45900821592659
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-04 15:55:18 +0100

    22 - Project: API Request to GitLab

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from post import Post
-from user import User
+import requests
 
-app_user_1 = User("joeriabbo@hotmail.com", "Joeri Abbo", "1234", "Devops engineer")
-app_user_1.get_user_info()
+response = requests.get("https://gitlab.com/api/v4/users/nanuchi/projects")
+my_projects = response.json()
 
-app_user_2 = User("example@gmail.com", "Example User", "1234", "Devops engineer")
-app_user_2.get_user_info()
-
-new_post = Post("On a secret mission today", app_user_2.name)
-new_post.get_post_info()
+for project in my_projects:
+    print(f"Project name: {project['name']} Project url: {project['web_url']}")
