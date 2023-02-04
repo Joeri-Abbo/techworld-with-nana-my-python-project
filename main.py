927b5fee1f19a6c22ef6dc87f4a9fb3f68653ca2
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-04 13:56:22 +0100

    7 - Functions

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 calculation_to_units = 24
 name_of_unit = "hours"
 
-print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
-print(f"35 days are {35 * calculation_to_units} {name_of_unit}")
-print(f"50 days are {50 * calculation_to_units} {name_of_unit}")
-print(f"110 days are {110 * calculation_to_units} {name_of_unit}")
+
+def days_to_units(num_of_days, custom_message):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+    print(custom_message)
+
+
+def scope_checks(num_of_days):
+    my_var = "variable inside functions"
+    print(name_of_unit)
+    print(num_of_days)
+    print(my_var)
+
+
+scope_checks(20)
+days_to_units(20, "Awesome!")
+days_to_units(35, "Looks good!")
