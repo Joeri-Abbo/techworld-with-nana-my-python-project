59903d1c853aa2590c8a95d275d97083e79ba3c4
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-04 13:59:22 +0100

    8 - Accepting User Input

--- main.py
+++ main.py
@@ -2,18 +2,11 @@ calculation_to_units = 24
 name_of_unit = "hours"
 
 
-def days_to_units(num_of_days, custom_message):
-    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
-    print(custom_message)
+def days_to_units(num_of_days):
+    return f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}"
 
 
-def scope_checks(num_of_days):
-    my_var = "variable inside functions"
-    print(name_of_unit)
-    print(num_of_days)
-    print(my_var)
-
-
-scope_checks(20)
-days_to_units(20, "Awesome!")
-days_to_units(35, "Looks good!")
+user_input = input("Hey user, enter a number of days and I will convert it to hours!\n")
+user_input_number = int(user_input)
+calculated_value = days_to_units(user_input_number)
+print(calculated_value)
