0e4f4aa5286b64ab76ecfaf3ce6c2ef8c3e8316a
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-05 15:00:48 +0100

    3 - Getting familiar with Boto

--- main.py
+++ main.py
@@ -1,10 +1,37 @@
 import boto3
 
-client = boto3.client(
+region = 'us-west-3'
+ec2_client = boto3.client(
     'ec2',
-    # region_name='us-east-1'
+    region_name=region
 )
-all_availability_vpcs = client.describe_vpcs()
+ec2_resource = boto3.resource(
+    'ec2',
+    region_name=region
+)
+
+new_vpc = ec2_resource.create_vpc(
+    CidrBlock='10.0.0.0/16',
+)
+
+new_vpc.create_subnet(
+    CidrBlock='10.0.1.0/24',
+)
+
+new_vpc.create_subnet(
+    CidrBlock='10.0.2.0/24',
+)
+
+new_vpc.create_tags(
+    Tags=[
+        {
+            "Key": "Name",
+            "Value": "my-vpc"
+        }
+    ]
+)
+
+all_availability_vpcs = ec2_client.describe_vpcs()
 vpcs = all_availability_vpcs["Vpcs"]
 
 for vpc in vpcs:
