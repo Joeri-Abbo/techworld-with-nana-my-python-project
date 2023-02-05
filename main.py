005f2a6ea8530e04b6a7e82a623290da5316f987
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-05 15:34:24 +0100

    6 - Write a Scheduled Task in Python

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import boto3
+import schedule
 
 region = 'eu-west-3'
 ec2_client = boto3.client(
@@ -10,9 +11,14 @@ ec2_resource = boto3.resource(
     region_name=region
 )
 
-statuses = ec2_client.describe_instance_status()
-for status in statuses['InstanceStatuses']:
-    ins_status = status["InstanceState"]["Name"]
-    sys_status = status["SystemStatus"]["Status"]
-    state = status["InstanceState"]["Name"]
-    print(f"Instance {status['InstanceId']} is {state} is {ins_status} and system status is {sys_status}")
+
+def check_instance_status():
+    statuses = ec2_client.describe_instance_status()
+    for status in statuses['InstanceStatuses']:
+        ins_status = status["InstanceState"]["Name"]
+        sys_status = status["SystemStatus"]["Status"]
+        state = status["InstanceState"]["Name"]
+        print(f"Instance {status['InstanceId']} is {state} is {ins_status} and system status is {sys_status}")
+
+
+schedule.every(5).minutes.do(check_instance_status)
