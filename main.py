d0b3e7efbe3c51eca76ab751f7ad052761745b40
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-05 15:28:29 +0100

    5 - Health Check: EC2 Status Checks

--- main.py
+++ main.py
@@ -10,32 +10,15 @@ ec2_resource = boto3.resource(
     region_name=region
 )
 
-new_vpc = ec2_resource.create_vpc(
-    CidrBlock='10.0.0.0/16',
-)
+instances = ec2_client.describe_instances()
 
-new_vpc.create_subnet(
-    CidrBlock='10.0.1.0/24',
-)
+for reservation in instances['Reservations']:
+    instances = reservation["Instances"]
+    for instance in instances:
+        print(f"Instance {instance['InstanceId']} is {instance['State']['Name']}")
 
-new_vpc.create_subnet(
-    CidrBlock='10.0.2.0/24',
-)
-
-new_vpc.create_tags(
-    Tags=[
-        {
-            "Key": "Name",
-            "Value": "my-vpc"
-        }
-    ]
-)
-
-all_availability_vpcs = ec2_client.describe_vpcs()
-vpcs = all_availability_vpcs["Vpcs"]
-
-for vpc in vpcs:
-    print(vpc["VpcId"])
-    cidr_block_association_set = vpc["CidrBlockAssociationSet"]
-    for cidr_block_association in cidr_block_association_set:
-        print(cidr_block_association["CidrBlockState"])
+statuses = ec2_client.describe_instance_status()
+for status in statuses['InstanceStatuses']:
+    ins_status = status["InstanceState"]["Name"]
+    sys_status = status["SystemStatus"]["Status"]
+    print(f"Instance {status['InstanceId']} is {ins_status} and system status is {sys_status}")
