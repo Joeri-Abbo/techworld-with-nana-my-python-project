7de1efe46361a9939d6d73eaad0be098186a281f
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-05 15:38:19 +0100

    6 - Write a Scheduled Task in Python

--- main.py
+++ main.py
@@ -13,12 +13,18 @@ ec2_resource = boto3.resource(
 
 
 def check_instance_status():
-    statuses = ec2_client.describe_instance_status()
+    statuses = ec2_client.describe_instance_status(
+        IncludeAllInstances=True,
+    )
     for status in statuses['InstanceStatuses']:
         ins_status = status["InstanceState"]["Name"]
         sys_status = status["SystemStatus"]["Status"]
         state = status["InstanceState"]["Name"]
         print(f"Instance {status['InstanceId']} is {state} is {ins_status} and system status is {sys_status}")
+    print("-" * 50)
 
 
-schedule.every(5).minutes.do(check_instance_status)
+schedule.every(5).seconds.do(check_instance_status)
+
+while True:
+    schedule.run_pending()
