3a3bf85d94c6f6e578c44bb2dd55802a66cd686a
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-05 15:30:05 +0100

    5 - Health Check: EC2 Status Checks

--- main.py
+++ main.py
@@ -10,15 +10,9 @@ ec2_resource = boto3.resource(
     region_name=region
 )
 
-instances = ec2_client.describe_instances()
-
-for reservation in instances['Reservations']:
-    instances = reservation["Instances"]
-    for instance in instances:
-        print(f"Instance {instance['InstanceId']} is {instance['State']['Name']}")
-
 statuses = ec2_client.describe_instance_status()
 for status in statuses['InstanceStatuses']:
     ins_status = status["InstanceState"]["Name"]
     sys_status = status["SystemStatus"]["Status"]
-    print(f"Instance {status['InstanceId']} is {ins_status} and system status is {sys_status}")
+    state = status["InstanceState"]["Name"]
+    print(f"Instance {status['InstanceId']} is {state} is {ins_status} and system status is {sys_status}")
