cbde5e86f2175001966826cdcf27281253d49dd3
Joeri Abbo <joeriabbo@hotmail.com>
2023-02-18 00:42:47 +0100

    14 - Website Monitoring 2: Automated Email Notification

--- monitor-website.py
+++ monitor-website.py
@@ -1,7 +1,35 @@
 import requests
+import smtplib
+import os
+
+EMAIL_ADDRESS = os.environ.get("EMAIL_ADDRESS", "EMAIL_ADDRESS")
+EMAIL_PASSWORD = os.environ.get("EMAIL_PASSWORD", "EMAIL_PASSWORD")
+
+
+def send_notification(msg):
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        smtp.sendmail(
+            EMAIL_ADDRESS,
+            EMAIL_ADDRESS,
+            msg
+        )
+
 
 response = requests.get('https://www.google.com')
-if response.status_code == 200:
-    print('Application is running successfully!')
-else:
-    print('Application is down. Fix it!')
+try:
+    if response.status_code == 200:
+        print('Application is running successfully!')
+    else:
+        print('Application is down. Fix it!')
+        send_notification(
+            'Subject: Application is down!\n\nApplication is down. Fix it!'
+        )
+
+except Exception as e:
+    print(e)
+    send_notification(
+        'Subject: Application is accessible at all!\n\nApplication is down. Fix it!'
+    )
